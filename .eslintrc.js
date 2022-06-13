442d557f4dc0bc661ed24f412ee8fc3afcb49e66
zonglinlee <173389705@qq.com>
2022-06-13 15:30:39 +0800

    配置 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential",
-        "plugin:prettier/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:prettier/recommended',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2020,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
     },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-};
+    plugins: ['vue'],
+    rules: {
+        'vue/multi-word-component-names': ['warn'],
+    },
+}

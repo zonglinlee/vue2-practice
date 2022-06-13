11f0f062526652cf53949215378ff24a4da2fa76
zonglinlee <173389705@qq.com>
2022-06-14 00:24:00 +0800

    将错误抛出到控制台

--- src/utils/imgErrorHandler.js
+++ src/utils/imgErrorHandler.js
@@ -6,6 +6,8 @@ function imgSrcHandleError(err) {
         img.src =
             url +
             `?message=${message}&name=${name}&column=${columnNumber}&line=${lineNumber}`
+        // 加这个 console.error 是为了将错误抛出到控制台
+        console.error(err)
     }
 }
 

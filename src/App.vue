e1be53f1700a19de8ce71cf6925ae86989e0b449
lizonglin <173389705@qq.com>
2023-04-12 10:43:17 +0800

    tweak: index page css

--- src/App.vue
+++ src/App.vue
@@ -2,65 +2,74 @@
     <div id="app">
         <div
             :class="{ 'top-menu': topMenu }"
-            class="css-demo"
+            class="css-demo bg-vue dark:bg-dark-800 text-white"
             @click="changeMenuPosition"
         >
-            <router-link class="link-item" to="/scroll-snap1">
+            <router-link
+                class="link-item border-b border-solid border-light-300 px-4"
+                to="/scroll-snap1"
+            >
                 仿微信列表滑动删除功能
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/flex1"
                 @click.native="topMenu = false"
                 >flex布局测试
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/button1"
                 @click.native="topMenu = false"
                 >CSS style1
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/element-height"
                 @click.native="topMenu = false"
                 >Vue获取元素尺寸
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/clip-path1"
                 @click.native="topMenu = false"
                 >svg clipPath动画1
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/img-error-report"
                 @click.native="topMenu = false"
                 >img-error-report
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/sourcemap-test"
                 @click.native="topMenu = false"
                 >sourceMapTest
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/animationIndex"
                 @click.native="topMenu = false"
                 >Animation
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/css1"
                 @click.native="topMenu = false"
                 >css1
             </router-link>
             <router-link
-                class="link-item"
+                class="link-item border-b border-solid border-light-300 px-4"
                 to="/ui-events"
                 @click.native="topMenu = false"
                 >UI events
+            </router-link>
+            <router-link
+                class="link-item border-b border-solid border-light-300 px-4"
+                to="/echarts"
+                @click.native="topMenu = false"
+                >Echarts
             </router-link>
         </div>
         <div style="margin-left: 180px; padding: 15px 0 0 15px">
@@ -76,9 +85,32 @@ export default {
     data() {
         return {
             topMenu: false,
+            clickCount: 0,
+            maxInterval: 800,
         }
     },
+    mounted() {
+        document.addEventListener('click', this.toggleDarkMode)
+    },
     methods: {
+        toggleDarkMode() {
+            if (this.clickCount === 0) {
+                this.startTime = Date.now()
+                this.clickCount = this.clickCount + 1
+            } else if (this.clickCount > 0 && this.clickCount < 5) {
+                const interval = Date.now() - this.startTime
+                if (interval > this.maxInterval) {
+                    this.clickCount = 0
+                    this.startTime = Date.now()
+                }
+                this.clickCount = this.clickCount + 1
+            } else if (this.clickCount === 5) {
+                document.documentElement.classList.toggle('dark')
+                this.clickCount = 0
+            } else {
+                this.clickCount = 0
+            }
+        },
         changeMenuPosition(event) {
             console.log(event.target)
             const href = event.target.getAttribute('href')
@@ -106,23 +138,24 @@ export default {
     left: 0;
     top: 0;
     bottom: 0;
-    background: #f1f1f1;
-    padding: 10px;
     overflow-y: auto;
     display: flex;
     flex-flow: column nowrap;
-    justify-content: space-around;
+    justify-content: flex-start;
+
     a.link-item {
         height: 50px;
+        line-height: 50px;
         text-decoration: none;
-        color: #2b3044;
         @include text-ellipsis();
         transition: 0.3s;
         flex-shrink: 0;
     }
+
     a.link-item:hover {
         color: dodgerblue;
     }
+
     a.link-item.active {
         color: dodgerblue;
     }
@@ -137,8 +170,7 @@ export default {
 
     .link-item {
         width: 180px;
-        border-right: 1px solid dodgerblue;
-        margin-bottom: 10px;
+        border-right: 1px solid white;
         height: 30px;
         line-height: 30px;
         text-align: center;
